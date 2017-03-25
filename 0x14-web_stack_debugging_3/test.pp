6626da2277f4bfe97d4d5c5ba7bcf01e3682a4bf
guptaNswati <sgupta1505@gmail.com>
2017-03-25 02:05:29 +0000

    updated file to use stdlib

--- 0x14-web_stack_debugging_3/test.pp
+++
@@ -1,6 +0,0 @@
-# Using strace fix apache 500 error and automate fix with puppet.
-file { 'fix_php':
-  path    => './tmp.txt',
-  line    => '/var/www/html/wp-includes/class-wp-locale.php',
-  match   => '^/var/www/html/wp-includes/class-wp-locale.phpp'
-}
