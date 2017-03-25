c9ef0b476420305ec2e051d6f6aca6610f272827
guptaNswati <sgupta1505@gmail.com>
2017-03-25 02:04:55 +0000

    updated file to use stdlib

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,14 @@
 # Using strace fix apache 500 error and automate fix with puppet.
-file_line { 'fix_php':
-  path  => '/var/www/html/wp-settings.php',
-  line  => '/var/www/html/wp-includes/class-wp-locale.php',
-  match => '^/var/www/html/wp-includes/class-wp-locale.phpp.*$'
+node default {
+  include stdlib
+  file_line { 'fix_php':
+    path  => '/var/www/html/wp-settings.php',
+    line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
+    match => '^require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );.*$'
+  }
+
+  service { 'apache2':
+    ensure => 'running',
+    enable => true
+  }
 }
