1da0c36628021bf8348cff5653b3302bb202b9f4
guptaNswati <sgupta1505@gmail.com>
2017-03-25 02:43:58 +0000

    changing to command

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,9 @@
 # Using strace fix apache 500 error and automate fix with puppet.
-node default {
-  include stdlib
-  file_line { 'fix_php':
-    path  => '/var/www/html/wp-settings.php',
-    line  => 'require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );',
-    match => '^require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );.*$'
-  }
+exec { 'fix_php':
+  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
+  path    => '/bin/sed'
+}
 
-  service { 'apache2':
-    ensure => 'running',
-    enable => true
-  }
+service { 'apache2':
+  restart => true
 }
