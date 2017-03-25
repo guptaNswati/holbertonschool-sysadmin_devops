62e0b358799a08cd1ee03eb9a0f66e33b5f613f5
guptaNswati <sgupta1505@gmail.com>
2017-03-25 03:09:16 +0000

    getiing rid of extras

--- 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,5 @@
 # Using strace fix apache 500 error and automate fix with puppet.
 exec { 'fix_php':
-  command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
-  path    => '/bin/sed'
-}
-
-service { 'apache2':
-  restart => true
+  command => "sed -ie 's/locale.phpp/locale.php/' /var/www/html/wp-settings.php",
+  path    => ['/bin']
 }
