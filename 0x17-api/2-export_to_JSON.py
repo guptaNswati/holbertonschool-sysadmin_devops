3618641e9280c40c19361b59aeb38c22fbfef912
guptaNswati <sgupta1505@gmail.com>
2017-04-18 05:09:04 +0000

    2

--- 0x17-api/2-export_to_JSON.py
+++ 0x17-api/2-export_to_JSON.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
 """
 This module using the given REST API, for a given employee ID, returns
-information about his/her TODO list progress.
+information about his/her TODO list progress and export data in json format.
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
+    try:
+        employee_id = int(sys.argv[1])
+    except (TyepError, Valuerror):
+        print("Given id is invalid")
+        sys.exit()
+    req = requests.get("https://jsonplaceholder.typicode.com/todos")
+    tasks = []
+    for task in req.json():
+        if task.get('userId') == employee_id:
+            tasks.append(task)
+
     req = requests.get("https://jsonplaceholder.typicode.com/users/")
-    for employee in req.json():
-        if employee.get('id') == int(sys.argv[1]):
-            print(employee)
-            break
+    for user in req.json():
+        if user.get('id') == employee_id:
+            name = user.get('username')
+    file_name = "{:d}.json".format(employee_id)
+    with open(file_name, 'w') as f:
+        f.write('{"' + str(employee_id) + '": [')
+        for task in range(0, len(tasks) - 1):
+            f.write('{"' + "task" + '": ' + '"' + tasks[task].get('title') +
+                    '", ' + '"completed": ' + str(tasks[task].get('completed'))
+                    + ", " + '"username": ' +  '"' + name + '"}, ')
+        f.write('{"' + "task" + '": ' + '"' + tasks[task].get('title') + '", '
+                + '"completed": ' + str(tasks[task].get('completed')) + ", "
+                + '"username": ' +  '"' + name + '"}]}')
